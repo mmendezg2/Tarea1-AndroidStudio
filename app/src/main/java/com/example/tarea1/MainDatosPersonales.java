faaa3a76d35540471bca074d0f5c119964cd9d60
mmendezg2 <maria.mendez2016@uteq.edu.ec>
2021-11-30 13:18:42 +0100

    2 Cambios Realizados

--- app/src/main/java/com/example/tarea1/MainDatosPersonales.java
+++ app/src/main/java/com/example/tarea1/MainDatosPersonales.java
@@ -3,6 +3,7 @@ package com.example.tarea1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainDatosPersonales extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainDatosPersonales extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_datos_personales);
+
+
+//Localizar los controlesTextView
+        TextView txtNombrePer = (TextView)findViewById(R.id.lblNombreP);
+        TextView txtGeneroPer = (TextView)findViewById(R.id.lblGeneroP);
+        TextView txtFechaNacPer = (TextView)findViewById(R.id.lblFechaNaciP);
+        TextView txtTelefonoPer = (TextView)findViewById(R.id.lblTelefonoP);
+//Recuperamos la información pasada en el intent
+        Bundle bundle = this.getIntent().getExtras();
+//Construimos el mensaje a mostrar
+        txtNombrePer.setText(bundle.getString("NOMBRE"));
+        txtGeneroPer.setText(bundle.getString("Sexo"));
+        txtFechaNacPer.setText(bundle.getString("FechaNac"));
+        txtTelefonoPer.setText(bundle.getString("Telefono"));
+
+
     }
 }
